6425610379dc7bf4c41f87faf2e32368bda2334f
mohammadiyan.hassan1377 <mohammadiyan.hassan1377@gmail.com>
2022-08-20 15:36:16 +0430

    the UUID abstrract model insted of user ID was added.

--- painless/models.py
+++ painless/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class UUIDBaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique = True)
+
+    class Meta:
+        abstract = True
