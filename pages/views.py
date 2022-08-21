44190cc0539f9cb489a0961709d831c8183b5db9
mohammadiyan.hassan1377 <mohammadiyan.hassan1377@gmail.com>
2022-08-21 13:58:25 +0430

    register and login views was created.

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import (
+    TemplateView
+)
+
+
+class Register(TemplateView):
+    template_name = 'register.html'
+    page_name = "Register"
+
+    def get_context_data(self, **kwargs):
+        context = super(Register, self).get_context_data(**kwargs)
+
+        return context
+
+
+class Login(TemplateView):
+    template_name = 'login.html'
+    page_name = "Login"
+
+    def get_context_data(self, **kwargs):
+        context = super(Login, self).get_context_data(**kwargs)
+
+        return context
